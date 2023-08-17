e49d4d2416d6b38303b23b7a4b0e001356da4d0c
Nick Wong <njwon4@gmail.com>
2023-08-17 22:28:50 +1000

    refactor: better dark mode styles for TryItOut

--- src/components/TryItOut.jsx
+++ src/components/TryItOut.jsx
@@ -37,22 +37,22 @@ export function TryItOut() {
   return (
     <div className="my-16 xl:max-w-none">
       <Heading level={2} id="try-it-out">
-        Try it out
+        Try it out!
       </Heading>
 
       <form className="flex w-full items-center" onSubmit={handleSubmit}>
-        <div className="rounded-l-lg border bg-zinc-200 p-3 text-lg">
+        <div className="rounded-l-lg bg-zinc-200 p-3 font-mono text-lg dark:bg-zinc-800 dark:text-zinc-100">
           https://me-api/api/v1/
         </div>
         <input
-          className="border border-l-0 p-3 text-lg"
+          className="p-3 font-mono text-lg ring-1 ring-inset ring-zinc-200 dark:text-zinc-100 dark:ring-zinc-600"
           type="text"
           value={requestInput}
           onChange={(e) => setRequestInput(e.target.value)}
         />
         <button
           type="submit"
-          className="rounded-r-lg border border-emerald-400 bg-emerald-400 p-3 font-bold text-white"
+          className="rounded-r-lg bg-zinc-900 p-3 text-white hover:bg-zinc-700 dark:bg-emerald-400/10 dark:text-emerald-400 dark:ring-1 dark:ring-inset dark:ring-emerald-400/20 dark:hover:bg-emerald-400/10 dark:hover:text-emerald-300 dark:hover:ring-emerald-300"
         >
           Submit
         </button>
