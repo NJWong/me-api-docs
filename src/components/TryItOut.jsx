dbfd7287ac6c3a739e52094016a34eb4acc241ff
Nick Wong <njwon4@gmail.com>
2023-08-18 12:25:29 +1000

    chore: update v1 api paths; refactor: interactive examples in TryItOut

--- src/components/TryItOut.jsx
+++ src/components/TryItOut.jsx
@@ -3,40 +3,37 @@
 import { useState, useEffect } from 'react'
 import { Heading } from '@/components/Heading'
 import { CodeGroup } from '@/components/Code'
+import { apiPathV1 } from '../consts/paths.js'
 
 export function TryItOut() {
   const [requestInput, setRequestInput] = useState('characters/2')
-  const [responsePath, setResponsePath] = useState(
-    'https://me-api.njwon4.workers.dev/api/characters/2'
-  )
+  const [responsePath, setResponsePath] = useState(`${apiPathV1}/characters/2`)
   const [response, setResponse] = useState('')
 
   useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(
-        `https://me-api.njwon4.workers.dev/api/${requestInput}`
-      )
-
-      const data = await res.json()
-
-      setResponse(data)
-      setResponsePath(`https://me-api.njwon4.workers.dev/api/${requestInput}`)
-    }
-
-    fetchData()
+    sendRequest(`${apiPathV1}/${requestInput}`)
   }, [])
 
   async function handleSubmit(e) {
     e.preventDefault()
 
-    const res = await fetch(
-      `https://me-api.njwon4.workers.dev/api/${requestInput}`
-    )
+    sendRequest(`${apiPathV1}/${requestInput}`)
+  }
+
+  async function handleClick(example) {
+    // Set the input field to the selected example
+    setRequestInput(example)
+
+    sendRequest(`${apiPathV1}/${example}`)
+  }
+
+  async function sendRequest(path) {
+    const res = await fetch(path)
 
     const data = await res.json()
 
     setResponse(data)
-    setResponsePath(`https://me-api.njwon4.workers.dev/api/${requestInput}`)
+    setResponsePath(path)
   }
 
   return (
@@ -46,8 +43,8 @@ export function TryItOut() {
       </Heading>
 
       <form className="flex w-full items-center" onSubmit={handleSubmit}>
-        <div className="rounded-l-lg bg-zinc-200 p-3 font-mono text-lg dark:bg-zinc-800 dark:text-zinc-100">
-          https://me-api.njwon4.workers.dev/api/
+        <div className="overflow-auto whitespace-nowrap rounded-l-lg bg-zinc-200 p-3 font-mono text-lg dark:bg-zinc-800 dark:text-zinc-100">
+          {apiPathV1}
         </div>
         <input
           className="p-3 font-mono text-lg ring-1 ring-inset ring-zinc-200 dark:text-zinc-100 dark:ring-zinc-600"
@@ -62,6 +59,27 @@ export function TryItOut() {
           Submit
         </button>
       </form>
+
+      <div>
+        <ul>
+          <li>
+            <button
+              className="font-bold text-emerald-500 hover:underline"
+              onClick={() => handleClick('characters?offset=5')}
+            >
+              characters?offset=5
+            </button>
+          </li>
+          <li>
+            <button
+              className="font-bold text-emerald-500 hover:underline"
+              onClick={() => handleClick('characters/4')}
+            >
+              characters/4
+            </button>
+          </li>
+        </ul>
+      </div>
 
       <div>
         <CodeGroup title="Response" tag="GET" label={responsePath}>
---
+++ src/consts/paths.js
@@ -0,0 +1,1 @@
+export const apiPathV1 = 'https://me-api.njwon4.workers.dev/api/v1'
